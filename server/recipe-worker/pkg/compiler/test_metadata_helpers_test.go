88c14a9a2cc2ebcc082ad37ea6ef7864388311c6
colony2.com <col2bot@colony2.com>
2025-12-08 13:18:20 -0800

    input activity work

--- server/recipe-worker/pkg/compiler/test_metadata_helpers_test.go
+++
@@ -1,7 +0,0 @@
-package compiler
-
-import "go.temporal.io/sdk/testsuite"
-
-// Temporal metadata signals are no longer used in tests; keep a no-op helper to
-// avoid refactoring callers while the suite migrates to SWF.
-func primeDefaultMetadataSignal(env *testsuite.TestWorkflowEnvironment) {}
